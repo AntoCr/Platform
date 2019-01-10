4befb111abb983e93d2cfa300b3c8c94bd87eeaf
AntoCr <add8596.@gmail.com>
2019-01-10 20:06:23 +0100

    newer version

--- miniPlayerData.js
+++ miniPlayerData.js
@@ -1,15 +1,1 @@
-klynt.miniPlayerData = {
-    "downloadAppWording": "Download App",
-    "thumbnail": "Medias/Thumbnails/yt_logo_rgb_dark.png.png",
-    "resumePlaybackWording": "Resume playback?",
-    "fullscreenInfoWording": "This program will launch in fullscreen",
-    "yesWording": "Yes",
-    "title": "Visioni Future",
-    "thanksForWatchingWording": "Thanks for watching!",
-    "noWording": "No",
-    "analyticsKey": "",
-    "redirectToMobileApp": "auto",
-    "launchAppWording": "Then Launch Project",
-    "url": "",
-    "description": "YouTube"
-}
+klynt.miniPlayerData = {"downloadAppWording":"Download App","title":"Visioni Future","thanksForWatchingWording":"Thanks for watching!","fullscreenInfoWording":"This program will launch in fullscreen","resumePlaybackWording":"Resume playback?","redirectToMobileApp":"auto","launchAppWording":"Then Launch Project","yesWording":"Yes","url":"","noWording":"No","thumbnail":"Player/css/editor/img/mini-player.jpg","analyticsKey":"","description":"YouTube"}
